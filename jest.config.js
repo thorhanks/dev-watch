1f5cb3006ef925f2298959f5b943e8e550016708
Thor Hanks <thor.hanks@gmail.com>
2021-06-11 17:37:57 -0700

    config: setting up actions and misc

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     preset: "@vue/cli-plugin-unit-jest/presets/no-babel",
     testMatch: ["**/?(*.)+(spec|test).[jt]s"],
+    collectCoverage: true,
+    collectCoverageFrom: ["<rootDir>/src/**/*.vue", "<rootDir>/src/**/*.js"],
+    coverageReporters: ["text-summary", "html", "lcov", "clover"],
+    coverageDirectory: "./docs/coverage",
 };
